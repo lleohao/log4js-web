ab71ff3d34496c5e261937a445bb011db7f4c604
lleohao <lleohao@hotmail.com>
2020-07-29 20:18:21 +0800

    test(categories): add categories test case

--- src/categories.ts
+++ src/categories.ts
@@ -1,21 +1,20 @@
 import { AppenderConfig, getAppender } from './appenders';
 import { Appender } from './appenders/appender';
-import { Configuration } from './configure/configure';
-import { Level, Levels, LevelType } from './levels';
+import { Configuration } from './configure';
 
-interface CategoryConfigure {
+/**
+ * category configure
+ */
+export interface CategoryConfigure {
   appenders: AppenderConfig[];
-  level?: LevelType;
 }
 
-class Category {
+export class Category {
   public name: string;
   public appenders: Appender[] = [];
-  public level: Level;
 
   constructor(name: string, configure: CategoryConfigure) {
     this.name = name;
-    this.level = Level.getLevel(configure.level, Levels.ALL);
 
     this.appenders = configure.appenders.map((appenderConfig) => {
       return getAppender(appenderConfig);
@@ -26,10 +25,20 @@ class Category {
 const categories: Map<string, Category> = new Map<string, Category>();
 
 /**
+ * Set default category
+ */
+categories.set(
+  'default',
+  new Category('default', {
+    appenders: [{ type: 'console' }],
+  })
+);
+
+/**
  * setup categories
  * @param config
  */
-const setupCategories = (config: Configuration): void => {
+export const setupCategories = (config: Configuration): void => {
   categories.clear();
 
   const categoryNames = Object.keys(config.categories);
@@ -44,22 +53,10 @@ const setupCategories = (config: Configuration): void => {
  * if category not defined, will return default category
  * @param category
  */
-const getCategoryByCategoryName = (category?: string): Category => {
+export const getCategoryByCategoryName = (category?: string): Category => {
   if (categories.has(category)) {
     return categories.get(category);
   }
 
   return categories.get('default');
 };
-
-/**
- * Set default category
- */
-categories.set(
-  'default',
-  new Category('default', {
-    appenders: [{ type: 'console' }],
-  })
-);
-
-export { Category, CategoryConfigure, setupCategories, getCategoryByCategoryName };
