774a6430b8101a182b2b45546d0bb5405d178a5a
lleohao <lleohao@hotmail.com>
2020-07-29 15:44:46 +0800

    chore(eslint): change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 0,
+  },
 };
