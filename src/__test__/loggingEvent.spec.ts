6020f773e859ac86dd012294f67102ef81dac7c6
lleohao <lleohao@hotmail.com>
2020-07-29 11:31:00 +0800

    feat(loggingEvent): simplify loggingEvent
    1. remove LoggingLocation

--- src/__test__/loggingEvent.spec.ts
+++ src/__test__/loggingEvent.spec.ts
@@ -25,34 +25,4 @@ describe('LoggingEvent test', () => {
     });
     expect(loggingEvent.context.user).toEqual('bob');
   });
-
-  test('should correct construct with/without location info', () => {
-    const callStack =
-      '    at repl:1:14\n    at ContextifyScript.Script.runInThisContext (vm.js:50:33)\n    at REPLServer.defaultEval (repl.js:240:29)\n    at bound (domain.js:301:14)\n    at REPLServer.runBound [as eval] (domain.js:314:12)\n    at REPLServer.onLine (repl.js:468:10)\n    at emitOne (events.js:121:20)\n    at REPLServer.emit (events.js:211:7)\n    at REPLServer.Interface._onLine (readline.js:280:10)\n    at REPLServer.Interface._line (readline.js:629:8)'; // eslint-disable-line
-    const fileName = '/log4js-node/test/tap/layouts-test.js';
-    const lineNumber = 1;
-    const columnNumber = 14;
-    const location = {
-      fileName,
-      lineNumber,
-      columnNumber,
-      callStack,
-    };
-
-    const event = new LoggingEvent('cheese', Levels.DEBUG, ['log message'], { user: 'bob' }, location);
-
-    expect(event.fileName).toEqual(fileName);
-    expect(event.lineNumber).toEqual(lineNumber);
-    expect(event.columnNumber).toEqual(columnNumber);
-    expect(event.callStack).toEqual(callStack);
-
-    const event2 = new LoggingEvent('cheese', Levels.DEBUG, ['log message'], {
-      user: 'bob',
-    });
-
-    expect(event2.fileName).toBeUndefined();
-    expect(event2.lineNumber).toBeUndefined();
-    expect(event2.columnNumber).toBeUndefined();
-    expect(event2.callStack).toBeUndefined();
-  });
 });
