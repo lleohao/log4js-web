5ed4a684b4385b025b891cdce18bc60accea6cb6
lleohao <lleohao@hotmail.com>
2020-09-08 14:03:04 +0800

    feat: export LoggingEvent

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { Configuration } from './configure';
 export { LevelType } from './levels';
 export { Logger } from './logger';
+export { LoggingEvent } from './loggingEvent';
 
 export { getLogger, configure, logger } from './log4js';
